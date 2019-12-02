dd3404f3bba3eaa904956db379f3fcdb52ff3731
sieg <sieg@zimutech.com>
2019-12-02 18:35:23 +0800

    v1.0

---
+++ src/AMap.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Zimutech;
+
+use GuzzleHttp\Client;
+
+require_once '../vendor/autoload.php';
+
+class AMap
+{
+    private $key;
+    private $client;
+
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+        $this->client = new Client();
+    }
+
+    public function getAdCodeFromIp(string $ip) : string
+    {
+        $output = $this->client
+            ->get('https://restapi.amap.com/v3/ip?parameters', [
+                'query' => [
+                    'key' => $this->key,
+                    'ip' => $ip
+                ]
+            ])
+            ->getBody()
+            ->getContents();
+
+        $output = json_decode($output, true);
+        $adcode = $output['adcode'];
+
+        if(!is_array($adcode)) {
+            return $adcode;
+        } else {
+            return '0';
+        }
+    }
+}
